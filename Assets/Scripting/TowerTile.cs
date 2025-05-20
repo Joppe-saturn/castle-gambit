6fa122e9d217fd61794f41a6b6048c77d7dd347c
JelledeLeeuw <jelledeleeuw@proton.me>
2025-05-20 10:33:21 +0200

    tiles update

--- Assets/Scripting/TowerTile.cs
+++ Assets/Scripting/TowerTile.cs
@@ -12,14 +12,17 @@ public class TowerTile : MonoBehaviour, IClickable
 
     public void OnClick()
     {
-        if (_dataManager.CurrentTower != null && placedTower == null)
+        if (_dataManager.CurrentTower != null)
         {
-            if (_dataManager.CurrentTower.Price <= _dataManager.Money)
+            if (placedTower == null)
             {
-                _dataManager.Money -= _dataManager.CurrentTower.Price;
-                placedTower = _dataManager.CurrentTower;
-                placedTower.gameObject = Instantiate(_dataManager.CurrentTower.gameObject, transform.position, Quaternion.identity);
-                _dataManager.CurrentTower = null;
+                if (_dataManager.CurrentTower.Price <= _dataManager.Money)
+                {
+                    _dataManager.Money -= _dataManager.CurrentTower.Price;
+                    placedTower = _dataManager.CurrentTower;
+                    placedTower.gameObject = Instantiate(_dataManager.CurrentTower.gameObject, transform.position, Quaternion.identity);
+                    _dataManager.CurrentTower = null;
+                }
             }
         }
     }
