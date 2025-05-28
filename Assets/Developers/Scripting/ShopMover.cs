693916e56fb3f2b849b42c71afbe4879ba097286
Joppe-saturn <potatendo@gmail.com>
2025-05-28 10:54:16 +0200

    movable shop ui

---
+++ Assets/Developers/Scripting/ShopMover.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class ShopMover : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 openPos;
+    [SerializeField] private Vector3 closePos;
+
+    public bool isOpen = true;
+
+    private RectTransform rectTransform;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void ChangeShopPos()
+    {
+        isOpen = !isOpen;
+
+        StartCoroutine(LerpShop());
+    }
+
+    private IEnumerator LerpShop()
+    {
+        if (isOpen)
+        {
+            while (rectTransform.localPosition != openPos && isOpen)
+            {
+                rectTransform.localPosition += (openPos - rectTransform.localPosition) / speed;
+                yield return new WaitForSeconds(0.02f);
+            }
+        } 
+        else
+        {
+            while (rectTransform.localPosition != closePos && !isOpen)
+            {
+                rectTransform.localPosition += (closePos - rectTransform.localPosition) / speed;
+                yield return new WaitForSeconds(0.02f);
+            }
+        }
+    }
+}
