c3497fcb35d5b23c1ebcf0c7fe5ebafbbd074793
JelledeLeeuw <jelledeleeuw@proton.me>
2025-06-11 13:01:15 +0200

    main

---
+++ Assets/Developers/Scripting/BishopProjectileSpin.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BishopProjectileSpin : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void FixedUpdate()
+    {
+        float _rotationY = transform.rotation.eulerAngles.y;
+        Debug.Log(_rotationY);
+        gameObject.transform.rotation = Quaternion.Euler(0f, _rotationY + _speed, 0f);
+    }
+}
